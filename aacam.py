f6e885a3d91fb59cfe0268351f885d9d5dd48904
src3453 <info@example.com>
2023-06-25 14:22:21 +0900

    modified:   .gitignore 	modified:   aacam.py 	modified:   aapy.py

--- aacam.py
+++ aacam.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import os
 from datetime import datetime
+import unicodedata
+
+
 
 # Webカメラを使うときはこちら
 cap = cv2.VideoCapture(0)
@@ -10,20 +13,34 @@ avg = None
 
 os.system("clear")
 est=0
+printer = aa.gray()
+#printer.chars = r" ▘▀▚▛█"
+    # ░▒▓█ 
+    # ▘▖▝▗▀▄▚▞▐▋▛▙▜▟█
+    # ▁▂▃▄▅▆▇█
+    # .-:=+*#%@
+    # ▏▎▍▌▋▊▉█
+    # ▖▖▚▚▜▜█
+    #  ⡀⠈⡐⠡⡘⡙⡛⣾⣿▒▓█
+    # ´-²;º=\LzïY[j1kéñü6Søq©AKÈRNÒÂÕ¶ / Latin-1
+
+    #printer.chars=["  ","▚ "," ▚","▚▚","█▚","▚█","██"]
+
+
 while True:
     # 1フレームずつ取得する。
     ret, frame = cap.read()
     if not ret:
         break
 #Place code here
-    REPL=2
+    REPL = 2
     termsize = os.get_terminal_size()
-    dst = cv2.resize(frame,(termsize[0]//REPL,min(int(termsize[0]/(16/9)/REPL),termsize[1])-1),interpolation=cv2.INTER_NEAREST)
-    printer = aa.gray()
-    printer.chars = " ▁▂▃▄▅▆▇█"# ░▒▓█  ▁▂▃▄▅▆▇█
+    
+    dst = cv2.resize(frame,(termsize[0],min(int(termsize[0]/(16/9)/REPL),termsize[1])-1),interpolation=cv2.INTER_NEAREST)
+    
     print(f"{est}fps",end="\r")
     pre = datetime.now()
-    printer.print(dst,chrrepl=REPL)
+    printer.print(dst,chrrepl=1)
     est = int(1/(datetime.now()-pre).total_seconds())
     #cv2.imshow("Main", dst)
     key = cv2.waitKey(10)
--- aapy.py
+++ aapy.py
@@ -5,10 +5,14 @@ import numpy as np
 CMODE_ONLYONE = True
 CMODE_CONTINUOUS = False
 
-class color:
+def getNearestIndex(lst, num):
+    idx = np.abs(np.asarray(lst) - list(num)).argmin()
+    return idx
+
+class color_full:
     def __init__(self) -> None:
         pass
-    def print(self,img:np.ndarray,loc:tuple[int,int]=(0,0),char:str="██",chrmode:bool=CMODE_ONLYONE,chrrepl:int=2) -> None:
+    def print(self,img:np.ndarray,loc:tuple[int,int]=(0,0),char:str="█",chrmode:bool=CMODE_ONLYONE,chrrepl:int=1) -> None:
         char_tmp = ""
         print(f"\033[{loc[0]};{loc[1]}H",end="")
         i=0
@@ -24,9 +28,30 @@ class color:
                         i+=1
                 print(f"\033[38;2;{img[y,x,2]};{img[y,x,1]};{img[y,x,0]}m{char_tmp}\033[0m",end="")
             print()
+
+CHARS=" .-:=+*?#&%@$"
+
+class color_std:
+    def __init__(self) -> None:
+        self.chars = CHARS
+    def print(self,img:np.ndarray,loc:tuple[int,int]=(0,0),chrrepl:int=1):
+        esc=["\033[31m","\033[32m","\033[34m"]
+        print(f"\033[{loc[0]};{loc[1]}H",end="")
+        tmp = np.mean([img.flatten() for i in range(0,len(img.flatten()),4)])
+        tmp=tmp//(256/len(self.chars))
+        chrs = np.array(list(self.chars))[tmp.astype(np.int0)]
+        k=0
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+                for i in range(chrrepl):    
+                    print(f"{esc[k%3]}{chrs[y*img.shape[1]+x]}",end="")
+                k+=1
+            print()
+
+
 class gray:
     def __init__(self) -> None:
-        self.chars=" .,:;+*?%$#@"
+        self.chars = CHARS
     def print(self,img:np.ndarray,loc:tuple[int,int]=(0,0),chrrepl:int=2):
         print(f"\033[{loc[0]};{loc[1]}H",end="")
         tmp = np.average(img,2)
